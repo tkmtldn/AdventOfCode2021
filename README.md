510b2e953301d2bfa783e134dccee32d987bcea4
Anton Agafonov <tkmtldn@gmail.com>
2021-12-01 13:13:06 +0400

    update Day 01

---
+++ README.md
@@ -0,0 +1,5 @@
+# AdventOfCode2021
+
+https://adventofcode.com/2021/
+
+My fifth attempt in annual problem solving competition.
---
+++ day_01/day01.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strconv"
+)
+
+func addToQue(q []int, x int) int {
+	q[0], q[1], q[2] = x, q[0], q[1]
+	return q[0] + q[1] + q[2]
+}
+
+func main() {
+	path := filepath.Join(".", "day_01", "input.txt")
+	fileHandle, _ := os.Open(path)
+	defer fileHandle.Close()
+
+	que := []int{0, 0, 0}
+	previous1, previous2 := 0, 0
+	ans1 := 0
+	ans2 := 0
+
+	fileScanner := bufio.NewScanner(fileHandle)
+	for fileScanner.Scan() {
+		cur, _ := strconv.Atoi(fileScanner.Text())
+
+		if cur > previous1 {
+			ans1++
+		}
+
+		curB := addToQue(que, cur)
+		if curB > previous2 {
+			ans2++
+		}
+
+		previous1 = cur
+		previous2 = curB
+	}
+
+	fmt.Printf("First answer: : %v \n", ans1 - 1)
+	fmt.Printf("Second answer: : %v \n", ans2 - 3)
+}
